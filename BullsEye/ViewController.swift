c109acea4cc9db8e6facfd82ee6cd5d1e732bb17
Akshay Tata <at758@cornell.edu>
2015-10-23 11:58:46 -0400

    All new changes

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue: Int = 50
+    @IBOutlet weak var sender: UISlider!
+    var targetValue: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        currentValue = lroundf(sender.value)
+        targetValue = 1 + Int(arc4random_uniform(100))
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,18 +28,29 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert(){
         
-        let alert = UIAlertController(title: "Hello world", message: "This is my first app!", preferredStyle: .Alert)
+        let newVal = "The current value of slider is \(currentValue) \n The target value is \(targetValue)"
+        
+        let alert = UIAlertController(title: "Hello world", message: newVal, preferredStyle: .Alert)
         
         let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
         
         alert.addAction(action)
+    
         
         presentViewController(alert, animated: true, completion: nil)
         
         
         
+        
+        
     }
-
+    
+   
+    @IBAction func slideMe(sender: UISlider) {
+        
+        currentValue = lroundf(sender.value)
+        
+    }
 
 }
 
