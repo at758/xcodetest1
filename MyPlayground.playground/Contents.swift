f7b7cea62b7e97f573c2083336b57a17ddfff3b8
Akshay Tata <at758@cornell.edu>
2016-01-23 14:51:56 -0500

    Added a new playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import SystemConfiguration
+
+var str = "Hello, playground"
+
+print("Hello World")
+
+print("dummy")
+
+5 + 6 * 8
+
